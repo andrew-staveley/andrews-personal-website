d4c9f243bc7fab67e88cafee85bac8f7e97f7112
Andrew <andrewstaveley14@gmail.com>
2023-09-06 12:03:44 -0400

    Removed JS relating to the 'Shrek' Button

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
-
-function shrekscript() {
-    document.getElementById("shrekDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
